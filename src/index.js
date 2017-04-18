4182c3bef87dc7e409e2625f95acd24b23869ccb
Alvarojoered <arojaspen@gmail.com>
2017-04-18 16:40:00 -0700

    First render app.js Create components Install Youtube API package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBoepjixgBmT19gd4-aTjetHd8PpJiInss';
+
 // Create a new component.
 // This component should produce HTML.
 
-const App = function () {
+const App = () => {
   return <div>Hi!</div>;
 }
 
-// Take this component's generated HTML and put it on the page
+
+// Take this component's generated HTML and put it on the page (DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
